d75247256b63be19590ceffb7342def3536c9623
Devin Ludwig and Tyler Ferrin <devin@>
2017-01-03 14:18:08 -0800

    clockity clock clock

--- js/clock-interface.js
+++ js/clock-interface.js
@@ -1,13 +1,44 @@
 var Clock = require('./../js/clock.js').clockModule;
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 $(document).ready(function() {
 
-  var newClock = new Clock();
-  newClock.hour = moment().format("H");
-  newClock.minute = moment().format("m");
+  days.forEach(function(day){
+    $("#days").append("<option>" + day + "</option>");
+  });
 
-  $("#hour").prepend(newClock.hour);
-  $("#minute").prepend(newClock.minute);
+    newClock = new Clock();
 
+    var timer = setInterval(function() {
+      if (newClock.alarm === true) {
+        $(".alarm").show();
+      }
+    newClock.day = moment().format("dddd");
+    newClock.hour = moment().format("H");
+    newClock.minute = moment().format("mm");
+    newClock.second = moment().format("ss");
+    newClock.ampm = moment().format("A");
+    $("#day").text(newClock.day);
+    $("#hour").text(newClock.hour + " :");
+    $("#minute").text(newClock.minute);
+    $("#seconds").text(newClock.second);
+    $("#ampm").text(newClock.ampm);
+    newClock.fuck();
+    }, 1000);
+
+  $('#new-alarm').submit(function(event) {
+    event.preventDefault();
+    day = $('#days').val();
+    minute = $('#alarm-minute').val();
+    hour = $('#alarm-hour').val();
+    var newAlarm = moment().hour(hour).minute(minute).day(day);
+    newClock.alarms.push(newAlarm);
+    $("#alarms").append("<li>" + newAlarm.format("dddd, h:mm") + "</li>");
+  });
+
+  $("#alarm-off").click(function() {
+    newClock.alarm = false;
+    $(".alarm").hide();
+  })
 
 });
--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,27 @@
 function Clock() {
-  this.hour = "";
-  this.minute = "";
-  this.alarm = "";
+  this.day = "";
+  this.hour;
+  this.minute;
+  this.second;
+  this.ampm;
+  this.alarms = [];
+  this.alarm = false;
 }
+
+Clock.prototype.fuck = function() {
+  var currentTime = moment();
+  var _this = this;
+  this.alarms.forEach(function(alarmTime) {
+    if (currentTime.format("dddd, H:mm") === alarmTime.format("dddd, H:mm"))  {
+      console.log("before:" + _this.alarm);
+      _this.alarm = true;
+      console.log(this.alarm);
+      console.log("this: " + this);
+      // debugger;
+    }
+  });
+  console.log(this.alarm);
+};
 
 
 exports.clockModule = Clock;
